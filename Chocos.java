79d5f8599211dec86f5ab4bb50bd3c403d166460
Valluri Gayathri <60134196+vallurigayathri@users.noreply.github.com>
2020-02-11 13:35:51 +0530

    Add files via upload

---
+++ Chocos.java
@@ -0,0 +1,16 @@
+package newyeargift;
+
+public class Chocos {
+	int weight ,price;
+	Chocolate(int weight, int price)
+	{
+		this.weight=weight;
+		this.price=price;
+	}
+	public int getPrice() {
+		return price;
+	}
+	public int getWeight() {
+		return weight;
+	}
+}
---
+++ Sweets.java
@@ -0,0 +1,16 @@
+package newyeargift;
+
+public class Sweets {
+   int weight,price;
+   Sweet(int weight,int price)
+   {
+	   this.weight=weight;
+	   this.price=price;
+   }
+   public int getPrice() {
+		return price;
+	}
+	public int getWeight() {
+		return weight;
+	}
+}
